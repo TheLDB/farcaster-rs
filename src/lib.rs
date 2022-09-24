53e07849cd653fe942861eb0d9a3676245c659e0
TheLDB <ldb@erikboles.com>
2022-09-24 16:02:57 -0600

    feat(initialization): added new() function to farcaster struct to connect to the smart contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use ethers::{core::{types::Address, abi::Abi}, providers::{Provider, Http}};
+
+pub struct Farcaster {
+    pub address: Address,
+    pub abi: Abi,
+    pub provider: Provider<Http>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Farcaster {
+    pub fn new(client: String) -> Self {
+        let address = "0xe3Be01D99bAa8dB9905b33a3cA391238234B79D1".parse::<Address>().unwrap();
+        let abi: Abi = serde_json::from_str(r#"[{"name":"getDirectoryUrl","inputs":[{"internalType":"bytes32","name":"username","type":"bytes32"}],"outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"addressToUsername","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"}]"#).unwrap();
+        let client = Provider::<Http>::try_from(client).unwrap();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Farcaster {
+            address,
+            abi,
+            provider: client
+        }
     }
 }
