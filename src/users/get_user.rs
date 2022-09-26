5695a88a1d1c1c14bea9beac28a23ccb687d7e8b
TheLDB <ldb@erikboles.com>
2022-09-25 20:09:52 -0600

    feat: Updated get_user return type to user::Root struct

--- src/users/get_user.rs
+++ src/users/get_user.rs
@@ -1,7 +1,7 @@
 use crate::Farcaster;
 use crate::types::user;
 impl Farcaster {
-    pub async fn get_user_by_username(self, username: String) -> Result<String, Box<dyn std::error::Error>> {
+    pub async fn get_user_by_username(self, username: String) -> Result<user::Root, Box<dyn std::error::Error>> {
         let name_registry_logs = self.get_name_registry_logs().await.unwrap();
  
         for i in name_registry_logs {
@@ -10,9 +10,8 @@ impl Farcaster {
                 let address = format!("0x{}", address.value.to_string());
                 println!("{}", format!("https://api.farcaster.xyz/v1/profiles/{}", address));
                 let user = reqwest::get(format!("https://api.farcaster.xyz/v1/profiles/{}", address)).await?.text().await?;
-                let test: user::Root = serde_json::from_str(&user).unwrap();
-                println!("{:?}", test);
-                return Ok(address)
+                let user: user::Root = serde_json::from_str(&user).unwrap();
+                return Ok(user)
             }
         }
 
