b14d1c732e77a6f6050befbf441bebd29f6a94bc
TheLDB <ldb@erikboles.com>
2022-09-26 10:53:11 -0600

    feat(get_casts): added pagination support1

--- src/users/get_casts.rs
+++ src/users/get_casts.rs
@@ -1,12 +1,25 @@
-use crate::{Farcaster, types::casts::Root};
+use crate::{Farcaster, types::casts::{Root}};
 
 impl Farcaster {
-    pub async fn get_casts(&self, username: String) -> Result<Root, Box<dyn std::error::Error>> {
+    pub async fn get_casts(&self, username: String, casts_per_page: i64, page: i64) -> Result<Root, Box<dyn std::error::Error>> {
         let user = Farcaster::get_user_by_username(self, username).await.unwrap();
         let address = user.result.user.address;
 
-        let user = reqwest::get(format!("https://api.farcaster.xyz/v1/profiles/{}/casts", address)).await?.text().await?;
+        let user = reqwest::get(format!("https://api.farcaster.xyz/v1/profiles/{}/casts?per_page={}", address, casts_per_page)).await?.text().await?;
         let casts: Root = serde_json::from_str(&user).unwrap();
-        Ok(casts)
+        if page > 0 {
+            let mut loopy = 0;
+            let mut new_user: String = String::from("");
+            while loopy != page {
+                new_user = reqwest::get(casts.meta.next.clone()).await?.text().await?;
+                loopy += 1;
+            }
+            
+            let casts: Root = serde_json::from_str(&new_user).unwrap();
+            return Ok(casts)
+        }
+        else {
+            return Ok(casts)
+        }
     }
 }
