b33ef0d9262841bcebbfd8d3f8c82f11d86c837b
TheLDB <ldb@erikboles.com>
2022-09-27 21:10:55 -0600

    feat: put publish_cast on hold, JWT's are angering me

--- src/casts/publish_cast.rs
+++ src/casts/publish_cast.rs
@@ -1,13 +1,21 @@
-use ethers::{signers::Wallet, prelude::k256::ecdsa::SigningKey};
-
+use ethers::{signers::{Wallet, Signer}, prelude::k256::{ecdsa::SigningKey}};
+use chrono::offset;
 use crate::Farcaster;
 
-async fn _auth_header() -> String {
-    "aaaa".to_string()
+
+async fn auth_header(wallet: Wallet<SigningKey>) {
+    let address = wallet.address();
+    let now = offset::Utc::now();
+    let timestamp = now.timestamp();
+    let expiry = timestamp + 60;
+    let signer = wallet.signer();
+    let signer = signer.verifying_key();
+    // Feature on hold- can't figure out how to create a valid JWT in rust :(
 }
+
 impl Farcaster {
     pub async fn publish_cast(wallet: Wallet<SigningKey>) -> Result<(), Box<dyn std::error::Error>> {
-        println!("{:?}", wallet);
+        let auth = auth_header(wallet).await;
         Ok(())
     }
 }
