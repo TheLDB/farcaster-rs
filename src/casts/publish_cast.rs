da775971334f3879c1bdcb0d548501f58af10e24
TheLDB <ldb@erikboles.com>
2022-09-27 21:12:01 -0600

    fix: underscore unused functions/variables in publish_castr.s

--- src/casts/publish_cast.rs
+++ src/casts/publish_cast.rs
@@ -1,21 +1,21 @@
-use ethers::{signers::{Wallet, Signer}, prelude::k256::{ecdsa::SigningKey}};
+use ethers::{signers::{Wallet, Signer}, prelude::k256::ecdsa::SigningKey};
 use chrono::offset;
 use crate::Farcaster;
 
 
 async fn auth_header(wallet: Wallet<SigningKey>) {
-    let address = wallet.address();
+    let _address = wallet.address();
     let now = offset::Utc::now();
     let timestamp = now.timestamp();
-    let expiry = timestamp + 60;
+    let _expiry = timestamp + 60;
     let signer = wallet.signer();
-    let signer = signer.verifying_key();
+    let _signer = signer.verifying_key();
     // Feature on hold- can't figure out how to create a valid JWT in rust :(
 }
 
 impl Farcaster {
     pub async fn publish_cast(wallet: Wallet<SigningKey>) -> Result<(), Box<dyn std::error::Error>> {
-        let auth = auth_header(wallet).await;
+        let _auth = auth_header(wallet).await;
         Ok(())
     }
 }
