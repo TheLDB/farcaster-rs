c7b9799dfda6b50f0f697fb2a49b98e65403895f
TheLDB <ldb@erikboles.com>
2022-09-29 09:18:11 -0600

    fix: added .idea to .gitignore

--- src/casts/publish_cast.rs
+++ src/casts/publish_cast.rs
@@ -2,15 +2,12 @@ use ethers::{signers::{Wallet, Signer}, prelude::k256::ecdsa::SigningKey};
 use chrono::offset;
 use crate::Farcaster;
 
-
 async fn auth_header(wallet: Wallet<SigningKey>) {
-    let _address = wallet.address();
-    let now = offset::Utc::now();
-    let timestamp = now.timestamp();
-    let _expiry = timestamp + 60;
-    let signer = wallet.signer();
-    let _signer = signer.verifying_key();
-    // Feature on hold- can't figure out how to create a valid JWT in rust :(
+    // let _address = wallet.address();
+    // let now = offset::Utc::now();
+    // let timestamp = now.timestamp();
+    // let _expiry = timestamp + 60;
+    // let aaa = wallet.signer();
 }
 
 impl Farcaster {
