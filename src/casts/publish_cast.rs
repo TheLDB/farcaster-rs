d2c6096b835f9aabeefd403f604156b553e7dbc0
TheLDB <ldb@erikboles.com>
2022-09-30 14:21:00 -0600

    feat+fix: working on publish_cast

--- src/casts/publish_cast.rs
+++ src/casts/publish_cast.rs
@@ -1,24 +1,30 @@
 use ethers::{signers::{Wallet, Signer}, prelude::k256::ecdsa::SigningKey};
 use chrono::offset::Utc;
+use jwt_compact::{TimeOptions, alg::Es256k};
 use crate::Farcaster;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct CustomClaims {
+    pub issuer: &'static SigningKey
+}
 
 async fn auth_header(wallet: Wallet<SigningKey>) -> Result<String, Box<dyn std::error::Error>> {
-    // * Retrieve Address
+    // // * Retrieve Address
     let _address = wallet.address();
 
-    // * Create an expiry date 60 seconds ahead
+    // // * Create an expiry date 60 seconds ahead
     let current_time = Utc::now().timestamp();
     let _expiry_time = current_time + 60;
 
-    // *Retrieve the private key - ??????
+    // // *Retrieve the private key - ??????
     let key: &SigningKey = wallet.signer();
-    println!("{:?}", key);
 
-    // * Sign the private key with ES256K - ???????
+    // // * Sign a JWT with the signer from above, and the ES256 header - ???????
+    let time_options = TimeOptions::default();
 
-    // * Sign a JWT with the signer from above, and the ES256 header - ???????
-
-    // * Return JWT
+    let key = Es256k::new(key);
+    // // * Return JWT
     Ok(String::from("???????"))
 }
 
