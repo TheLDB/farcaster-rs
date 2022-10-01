f56d01d2837ccb0ab920f3958c2dadedd30d5862
TheLDB <ldb@erikboles.com>
2022-10-01 16:58:50 -0600

    docs: add docs for abis

--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -1,1 +1,22 @@
+/// ## Get 1 of the two Registry ABIs
+    /// 
+    /// Get either the ID or Name registry ABI's (for the FC V2 smart contracts on Goerli)
+    /// 
+    /// ## Arguments
+    /// 
+    /// * `registry`: types::abi::registry::Registry
+    ///     - Registry is an enum which has two types:
+    ///         - ID (Represents the ID registry ABI)
+    ///         - NAME (Represents the Name registry ABI)
+    /// 
+    /// ## Return Type
+    /// ``-> Option<&'static str>``
+    ///     - Returns an Optional String 
+    /// 
+    /// ## Usage
+    /// ```
+    /// use farcaster_rs::{Farcaster, types::abi::registry::Registry};
+    /// 
+    /// let registry = Farcaster::get_registry_abi(Registry::ID).unwrap();
+    /// ```
 pub mod get_registry_abi;
