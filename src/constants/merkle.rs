de5e7176666c6bdabdc5482fcfa5de121800b7eb
MistApproach <mistapproach@protonmail.com>
2022-11-04 00:29:21 +0100

    Corrected default duration

--- src/constants/merkle.rs
+++ src/constants/merkle.rs
@@ -4,4 +4,4 @@
 pub static API_ROOT: &str = "https://api.farcaster.xyz";
 
 /// default duration of authentication token in seconds
-pub const AUTH_TOKEN_DEFAULT_DURATION_SECS: i64 = 60 * 60 * 60; // 1 hour
+pub const AUTH_TOKEN_DEFAULT_DURATION_SECS: i64 = 60 * 60; // 1 hour
