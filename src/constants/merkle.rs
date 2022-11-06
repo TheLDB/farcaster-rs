5f909694ee06bb27dbd5869cbe89e344fb4962aa
MistApproach <mistapproach@protonmail.com>
2022-11-06 18:04:20 +0100

    Readability

--- src/constants/merkle.rs
+++ src/constants/merkle.rs
@@ -1,7 +1,8 @@
-/// Merkle API constants
+/// Merkle API v2 constants
 
 /// Merkle API root
 pub static API_ROOT: &str = "https://api.farcaster.xyz";
 
+/// Merkle API Authentication
 /// default duration of authentication token in seconds
 pub const AUTH_TOKEN_DEFAULT_DURATION_SECS: i64 = 60 * 60; // 1 hour
