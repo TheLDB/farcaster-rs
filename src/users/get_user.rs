bed572dea357c8ad19292112965ed549e3754b51
MistApproach <mistapproach@protonmail.com>
2022-11-06 18:06:28 +0100

    Prepare for V2 upgrade

--- src/users/get_user.rs
+++ src/users/get_user.rs
@@ -1,4 +1,5 @@
-use crate::types::user::user::UserRoot;
+//use crate::constants::merkle::API_ROOT;
+//use crate::types::user::user::UserRoot;
 use crate::types::user::UserInfo;
 use crate::Farcaster;
 use std::error::Error;
@@ -6,18 +7,19 @@ use std::error::Error;
 impl Farcaster {
     /// get the details of a user for a given `username`
     pub async fn get_user_by_username(&self, username: &str) -> Result<UserInfo, Box<dyn Error>> {
-        if let Some(address) = self.registry.get_address_by_username(username) {
+        if let Some(_fid) = self.registry.get_fid_by_username(username) {
+
             // TODO: this is still v1 API !
-            let user = reqwest::get(format!("https://api.farcaster.xyz/v1/profiles/{}", address))
-                .await?
-                .text()
-                .await?;
-            let user_root: UserRoot = serde_json::from_str(&user)?;
-            return Ok(user_root.result.user);
+            //let user = reqwest::get(format!("https://api.farcaster.xyz/v1/profiles/{}", address))
+            //    .await?
+            //    .text()
+            //    .await?;
+            //let user_root: UserRoot = serde_json::from_str(&user)?;
+            //return Ok(user_root.result.user);
         }
 
         Err(Box::from(format!(
-            "User '{}' not found in FNR registry",
+            "User '{}' not found in Registry",
             username
         )))
     }
