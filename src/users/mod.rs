a4eb291badb8973d5f6d8c4eba901575deeb52ac
MistApproach <mistapproach@protonmail.com>
2022-11-06 12:34:01 +0100

    Comment out broken v1 endpoint functions

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,5 +1,5 @@
-pub mod get_casts;
-pub mod get_followers;
-pub mod get_notifications;
-pub mod get_user;
-pub mod get_verified_address;
+//pub mod get_casts;
+//pub mod get_followers;
+//pub mod get_notifications;
+//pub mod get_user;
+//pub mod get_verified_address;
